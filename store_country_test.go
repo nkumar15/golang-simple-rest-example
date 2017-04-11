d4558083809e3023fa1e134b841dffc566e64a26
Neeraj <nir.neeraj@gmail.com>
2017-04-11 11:40:44 +0800

    formatted code in store country test

--- store_country_test.go
+++ store_country_test.go
@@ -1,6 +1,7 @@
 package location
 
 import (
+	"errors"
 	"testing"
 )
 
@@ -15,94 +16,94 @@ var countries = []Country{
 	},
 }
 
-func showMsg(name string, fail bool, t *testing.T) {
-	if fail {
-		t.Error(name + " failed.")
-	} else {
-		t.Log(name + " successful.")
-	}
-}
+func showMsg(err error, t *testing.T) {
 
-func showMsgWrapper(name string, err error, t *testing.T) {
-	showMsg(name, err != nil, t)
+	if err != nil {
+		t.Error(t.Name() + " failed with error" + err.Error())
+	} else {
+		t.Log(t.Name() + " successful.")
+	}
 }
 
 func Test_ConnectDB(t *testing.T) {
+
 	_, err := ConnectDB()
-	showMsgWrapper(t.Name(), err, t)
+	showMsg(err, t)
 }
 
 func Test_DeleteCountries(t *testing.T) {
+
 	err := DeleteCountries()
-	showMsgWrapper(t.Name(), err, t)
+	showMsg(err, t)
 }
 
 func Test_CreateCountry(t *testing.T) {
+
 	_, err := CreateCountry(countries[0])
-	showMsgWrapper(t.Name(), err, t)
+	showMsg(err, t)
 }
 
 func Test_GetCountry(t *testing.T) {
+
 	country, err := GetCountry(countries[0].Code)
-	showMsg(t.Name(), err != nil || country.Name != countries[0].Name, t)
+	if err != nil {
+		showMsg(err, t)
+	} else if country.Name != countries[0].Name {
+		showMsg(errors.New("Country name mismatch"), t)
+	}
 }
 
 func Test_GetCountries(t *testing.T) {
+
 	_, err := CreateCountry(countries[1])
-	if err != nil {
-		showMsg(t.Name(), true, t)
-	}
+	showMsg(err, t)
 
 	var cntries []Country
 	cntries, err = GetCountries()
 
-	if err != nil {
-		showMsg(t.Name(), true, t)
-	}
+	showMsg(err, t)
 	if cntries[0].Code != countries[0].Code || cntries[0].Name != countries[0].Name ||
 		cntries[1].Code != countries[1].Code || cntries[1].Name != countries[1].Name {
-		showMsg(t.Name(), true, t)
+		showMsg(errors.New("Country name code mismatch"), t)
 	}
-
-	showMsg(t.Name(), false, t)
+	showMsg(nil, t)
 }
 
 func Test_UpdateCountry(t *testing.T) {
+
 	var changedCountry Country
 	changedCountry.Code = countries[0].Code
 	changedCountry.Name = "Hindustan"
 
 	err := UpdateCountry(changedCountry)
-	if err != nil {
-		showMsg(t.Name(), true, t)
-	}
+	showMsg(err, t)
 
 	var country Country
 	country, err = GetCountry(changedCountry.Code)
 
-	if err != nil {
-		showMsg(t.Name(), true, t)
-	}
+	showMsg(err, t)
 
 	if country.Name != "Hindustan" {
-		showMsg(t.Name(), true, t)
+		showMsg(errors.New("Country name mismatch"), t)
 	}
 
 	changedCountry.Name = countries[0].Name
 	err = UpdateCountry(changedCountry)
-	if err != nil {
-		showMsg(t.Name(), true, t)
-	}
+	showMsg(err, t)
 
 	country, err = GetCountry(changedCountry.Code)
 
-	if err != nil {
-		showMsg(t.Name(), true, t)
-	}
+	showMsg(err, t)
 
 	if country.Name != countries[0].Name {
-		showMsg(t.Name(), true, t)
+		showMsg(errors.New("Country name mismatch"), t)
 	}
 
-	showMsg(t.Name(), false, t)
+	showMsg(nil, t)
+}
+
+func Test_DeleteCountry(t *testing.T) {
+	code := countries[1].Code
+	err := DeleteCountry(code)
+	showMsg(err, t)
 }
