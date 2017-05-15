b23cb756322688845644ef53d3902dbf749cddbd
Neeraj <nir.neeraj@gmail.com>
2017-05-15 19:00:46 +0800

    goose script for countries and users

--- db/migrations/sqlite3/20170515185748_create_country.sql
+++ db/migrations/sqlite3/20170515185748_create_country.sql
@@ -1,3 +1,6 @@
+
+-- +goose Up
+-- SQL in section 'Up' is executed when this migration is applied
 CREATE TABLE `Countries` (
 	`Id`	INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
 	`Code`	char(2) NOT NULL UNIQUE,
@@ -6,4 +9,7 @@ CREATE TABLE `Countries` (
 	`UpdatedAt`	timestamp NOT NULL,
 	`DeletedAt`	timestamp DEFAULT NULL
 );
--- +
+
+-- +goose Down
+-- SQL section 'Down' is executed when this migration is rolled back
+DROP TABLE Countries;
---
+++ db/migrations/sqlite3/20170515185846_create_users.sql
@@ -0,0 +1,15 @@
+
+-- +goose Up
+-- SQL in section 'Up' is executed when this migration is applied
+CREATE TABLE `Users` (
+	`Id`	INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+	`SessionId`	TEXT NOT NULL,
+	`UserId`	INTEGER NOT NULL,
+	`SessionStart`	TEXT NOT NULL,
+	`SessionUpdate`	TEXT NOT NULL,
+	`SessionActive`	NUMERIC
+);
+
+-- +goose Down
+-- SQL section 'Down' is executed when this migration is rolled back
+DROP TABLE Users;
